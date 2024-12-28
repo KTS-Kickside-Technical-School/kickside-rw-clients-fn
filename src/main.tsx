d23055e669bbc4e85cf8b8826b5e7e5a2d904522
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-12-28 21:41:58 +0200

    CH: Starts setting redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
 
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+
+const store = createStore()
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </Provider>
 )
