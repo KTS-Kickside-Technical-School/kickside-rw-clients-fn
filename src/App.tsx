582bd7d7228f2570714c5a37e369106bc084d70e
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-12-30 08:34:44 +0200

    Ft: SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from './routes'
+import { HelmetProvider } from 'react-helmet-async'
 
 const App: React.FC = () => {
 
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </HelmetProvider >
   )
 }
 
