aa4794328e14c916b1904bb5edfbb2797c67ef48
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-12-31 21:08:28 +0200

    Start editor

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
+import Editor from "./pages/Editor"
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Homepage />} />
+            <Route path="/editor" element={<Editor />} />
         </Routes>
     )
 }
