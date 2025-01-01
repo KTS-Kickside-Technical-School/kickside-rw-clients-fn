f3cef2db398e37cda9ce36ac5fa6d8529debc59d
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-01-01 23:09:54 +0200

    Fix: BUild issues

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 
 import Homepage from "./pages/Homepage"
-import Editor from "./pages/Editor"
 import ArticleDetails from "./pages/ArticleDetails"
 
 const AppRouter = () => {
@@ -9,7 +8,6 @@ const AppRouter = () => {
         <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/:slug" element={<ArticleDetails />} />
-            <Route path="/editor" element={<Editor />} />
         </Routes>
     )
 }
