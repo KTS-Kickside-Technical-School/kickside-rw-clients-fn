f3287ff8106fbbcc4da13f322e21119fca0a2110
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-01-23 06:01:20 +0200

    Fix auth issues (#36)
    * Fix:Remove env
    
    * Fix: fetch profile
    
    * Fix: Build issues

--- src/Components/staff/AuthGuard.tsx
+++ src/Components/staff/AuthGuard.tsx
@@ -1,10 +1,18 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-interface IsAuthenticated {
-    isAuthenticated: boolean;
-}
-const AuthGuard = ({ isAuthenticated }: IsAuthenticated) => {
-    return isAuthenticated ? <Outlet /> : <Navigate to="/staff/login" />;
+const AuthGuard = ({ isAuthenticated, fetchUserProfile }: any) => {
+  useEffect(() => {
+    if (isAuthenticated) {
+      fetchUserProfile().catch((error: any) => {
+        console.error('Unknown error while fetchnig profile', error);
+        toast.error('Failed to fetch user profile. Please log in again.');
+      });
+    }
+  }, [isAuthenticated, fetchUserProfile]);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/staff/login" />;
 };
 
 export default AuthGuard;
