05c477a476f0ed7933c50ad381a5b7e562c71f94
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-02-03 11:08:07 +0200

    Fix: Articles by categories (#53)

--- src/Components/Newsletter.tsx
+++ src/Components/Newsletter.tsx
@@ -1,26 +1,25 @@
 const NewsLetter: React.FC = () => {
   return (
-    <div className="w-full bg-[#3E60F4] py-16 pt-5">
-      <div className="w-full md:w-[80%] m-auto  text-white">
-        <div className="container mx-auto px-4">
-          <h2 className="text-4xl sm:text-2xl font-semibold text-white mb-2">
+    <div className="w-full bg-[#3E60F4] py-16 mt-10">
+      <div className="container mx-auto px-6">
+        <div className="max-w-lg mx-auto text-center text-white">
+          <h2 className="text-3xl sm:text-2xl font-semibold mb-3">
             Subscribe to our Newsletter
           </h2>
-          <p className="text-sm text-white mb-6">
+          <p className="text-sm mb-6">
             Stay updated with the latest from Kickside. You can unsubscribe
             anytime.
           </p>
 
-          <form className="flex flex-col sm:flex-row gap-0">
+          <form className="flex flex-col sm:flex-row items-center gap-4">
             <input
               type="email"
-              placeholder="Enter your email address"
-              className="w-[300px] bg-transparent sm:w-3/4 p-2 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-white transition-all"
+              placeholder="Enter your email"
+              className="w-full sm:w-2/3 px-4 py-3 bg-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all"
             />
-
             <button
               type="submit"
-              className="w-full sm:w-1/4 py-4  bg-white text-primary font-semibold transition-all focus:ring-2 focus:ring-blue-500"
+              className="w-full sm:w-1/3 px-6 py-3 bg-white text-blue-600 font-semibold hover:bg-gray-100 transition-all focus:ring-2 focus:ring-blue-300"
             >
               Subscribe
             </button>
