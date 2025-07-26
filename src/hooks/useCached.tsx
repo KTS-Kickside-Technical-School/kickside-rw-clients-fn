57ced42224bc7bfe69858d4cc09d44024d49ae2c
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-07-26 11:36:55 +0800

    Add build

--- src/hooks/useCached.tsx
+++ src/hooks/useCached.tsx
@@ -1,15 +1,14 @@
-// hooks/useCachedFetch.js or .ts
 import { useEffect, useState } from 'react';
 import { getCachedValue, setCachedValue } from '../utils/cacheStore';
 
-function useCachedFetch({ key, fetcher, ttl = 300000 }) {
+function useCachedFetch({ key, fetcher, ttl = 300000 }: any) {
   const [data, setData] = useState(() => getCachedValue(key));
   const [loading, setLoading] = useState(data === null);
 
   useEffect(() => {
     if (!data) {
       fetcher()
-        .then((res) => {
+        .then((res: any) => {
           setCachedValue(key, res, ttl);
           setData(res);
         })
