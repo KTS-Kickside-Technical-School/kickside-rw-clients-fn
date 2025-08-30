08004ac08777ee549eb84431ea56c63d1a8772a5
Ireme-Nyawe <akimana.inono@gmail.com>
2025-08-30 20:49:28 +0200

    create Kin versionof mainKsAd

--- src/pages/KinHomepage.tsx
+++ src/pages/KinHomepage.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import SEO from '../utils/SEO';
-import Footer from '../component/Footer';
-import NewsLetter from '../component/Newsletter';
-import MainTopKSAd from '../component/ads/MainTopKSAd';
-import Hero from '../component/clients/homepage/Hero';
-import MostPopular from '../component/MostPopularArticle';
-import AdvertisementSection from '../component/AdvertisementSection';
-import HomeLatestNews from '../component/HomeLatestNews';
-import HomeProductsCategories from '../component/HomeProductsCategories';
-import TrendsMatches from '../component/TrendsMatches';
-import KinHeader from '../component/KinHeader';
+import SEO from "../utils/SEO";
+import Footer from "../component/Footer";
+import NewsLetter from "../component/Newsletter";
+import MainTopKSAd from "../component/ads/MainTopKSAd";
+import Hero from "../component/clients/homepage/Hero";
+import MostPopular from "../component/MostPopularArticle";
+import AdvertisementSection from "../component/AdvertisementSection";
+import HomeLatestNews from "../component/HomeLatestNews";
+import HomeProductsCategories from "../component/HomeProductsCategories";
+import TrendsMatches from "../component/TrendsMatches";
+import KinHeader from "../component/KinHeader";
+import KinMainTopKSAd from "../component/ads/KinMainTopKSAd";
 
 const KinHomepage: React.FC = () => {
   return (
@@ -21,16 +22,15 @@ const KinHomepage: React.FC = () => {
             "Ahabanza: Kickside News - Isoko ya mbere y'imikino igezweho n'ibyamamare mu Rwanda. Ibishya mu Ikoranabuhanga byose ni hano wabisanga",
           description:
             "Kickside News ikinyamakuru cyambere cya Siporo usangaho amakuru agezweho y'imikino ndetse n'ubusesenguzi ku Byamamare ndetse n'Ikoranabuhanga. ",
-          author: 'Kickside Rwanda',
-          image: 'https://www.kickside.rw/logo.svg',
-          publishedAt: '2023-12-01T10:00:00Z',
-          type: 'website',
+          author: "Kickside Rwanda",
+          image: "https://www.kickside.rw/logo.svg",
+          publishedAt: "2023-12-01T10:00:00Z",
+          type: "website",
         }}
         canonicalUrl="https://www.kickside.rw/"
       />
 
-      <MainTopKSAd />
-
+      <KinMainTopKSAd />
       <div className="bg-primary pb-5">
         <KinHeader />
       </div>
